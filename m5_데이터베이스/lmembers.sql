ead68a0d567d3afe189f19b2f2270e1de10d4c84
hen77 <lynn5863@hotmail.com>
2022-10-04 17:36:09 +0900

    1004

--- m5_데이터베이스/lmembers.sql
+++ m5_데이터베이스/lmembers.sql
@@ -1,2 +1,24 @@
 --lmembers
 SELECT * FROM prodcl;
+SELECT * FROM channel;
+SELECT * FROM competitor;
+SELECT * FROM demo;
+SELECT * FROM member;
+SELECT * FROM purprd;
+
+
+SELECT mname
+FROM member
+GROUP BY mname;
+
+SELECT asso
+FROM channel
+GROUP BY asso;
+
+SELECT custno, SUM(puramt)
+FROM purprd
+GROUP BY custno;
+
+SELECT mcl
+FROM prodcl
+GROUP BY mcl;
