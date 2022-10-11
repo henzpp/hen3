cb288156f50ac06a852b9ce03552d9c9fa2f1267
hen77 <lynn5863@hotmail.com>
2022-10-11 09:11:00 +0900

    1011

--- m5_데이터베이스/lmembers.sql
+++ m5_데이터베이스/lmembers.sql
@@ -155,8 +155,14 @@ AND TO_CHAR(TO_DATE(purprd.purdate),'yyyymm') ='201405';
 SELECT SUBSTR(purprd.purdate,1,6) 월별, purprd.asso 제휴사, prodcl.mcl 중분류명, prodcl.scl 소분류명
 FROM prodcl, purprd
 WHERE prodcl.asso=purprd.asso 
-AND TO_CHAR(TO_DATE(purprd.purdate),'yyyymm') ='201406';
+AND TO_CHAR(TO_DATE(purprd.purdate),'yyyymm') ='201406'
+AND purprd.asso = 'B';
+-- A 백화점
+-- B 마트 - 온라인?? 종류 다양 -> 하이마트 어디감?
+-- C 슈퍼
+-- D 롭스
 
+SELECT * FROM CHANNEL;
 
 -- 중분류명 count
 SELECT COUNT(mcl) AS cnt, mcl 중분류명
@@ -190,10 +196,110 @@ FROM channel
 GROUP BY custno;
 
 
+--채널+이용횟수
+SELECT asso 제휴사, SUM(freq) "이용횟수 합"
+FROM channel
+GROUP BY asso
+ORDER BY SUM(freq) DESC;
 
 
+--프로덕트 : 제일 많이 이용한 제휴사 조회
+SELECT asso, COUNT(*)
+FROM prodcl
+GROUP BY asso
+ORDER BY COUNT(*) DESC;
+
+
+--멤버십 : 가입수가 많은 달 순서로 정렬
+SELECT mname,joinmonth,  COUNT(*)
+FROM member
+GROUP BY mname,joinmonth
+ORDER BY COUNT(*) DESC;
+
+
+
+
+
+
+
+
+
+
+
+--멤버십 : 가입수가 많은 달 순서로 정렬
+SELECT mname,  COUNT(*)
+FROM member
+GROUP BY mname
+ORDER BY mname,COUNT(*) DESC;
+
+--고객분류기준 정하기
+SELECT RNO,CUSTNO,PURDATE,SUM(PURAMT)
+FROM purprd
+GROUP BY RNO,CUSTNO,PURDATE
+ORDER BY CUSTNO ASC, PURDATE DESC;
+
+SELECT CUSTNO 고객번호, SUBSTR(purdate,1,6) 월별, SUM(puramt) 구매합계, ROUND(AVG(puramt),0) 평균구매, COUNT(puramt) 구매빈도
+FROM purprd
+GROUP BY CUSTNO,SUBSTR(purdate,1,6)
+ORDER BY CUSTNO,SUBSTR(purdate,1,6);
+
+-- 빈도수 기준 : 꾸준하게 구매한 충성 고객(최소 2달에 한번) / 빈도수가 줄어든 고객 / 빈도수가 늘어난 고객
+-- 구매합계 기준 : 고가 / 중가 / 저가
+
+--고객별 구매빈도
+SELECT CUSTNO 고객번호, SUM(puramt) 구매합계, ROUND(AVG(puramt),0) 평균구매, COUNT(puramt) 구매빈도
+FROM purprd
+GROUP BY CUSTNO
+ORDER BY CUSTNO;
+
+--고객별 평균 구매 단가 : 구매합계 / 구매빈도
+SELECT CUSTNO 고객번호, SUM(puramt) 구매합계, COUNT(puramt) 구매빈도, ROUND((SUM(puramt)/COUNT(puramt)),0) 평균구매단가
+FROM purprd
+GROUP BY CUSTNO
+ORDER BY CUSTNO;
+
+--고객별 첫 구매일
+SELECT CUSTNO 고객번호, TO_DATE(MIN(purdate)) 첫구매일
+FROM purprd
+GROUP BY CUSTNO
+ORDER BY CUSTNO;
+
+--고객별 마지막 구매일
+SELECT CUSTNO 고객번호, TO_DATE(MAX(purdate)) 마지막구매일
+FROM purprd
+GROUP BY CUSTNO
+ORDER BY CUSTNO;
+
+--CLTV : 평균 구매 단가 * 구매 빈도 * 계속 구매 기간
+SELECT CUSTNO 고객번호, ROUND((SUM(puramt)/COUNT(puramt)),0) 평균구매단가, COUNT(puramt) 구매빈도, TO_DATE(MAX((purdate)))-TO_DATE(MIN((purdate))) 계속구매기간,
+       (ROUND((SUM(puramt)/COUNT(puramt)),0) * COUNT(puramt) * ((TO_DATE(MAX(purdate)))-(TO_DATE(MIN(purdate))))) CLTV
+FROM purprd
+GROUP BY CUSTNO
+ORDER BY CLTV DESC;
+
+--VW_CLTV TABLE 생성
+CREATE VIEW VW_CLTV
+AS SELECT CUSTNO 고객번호, ROUND((SUM(puramt)/COUNT(puramt)),0) 평균구매단가, COUNT(puramt) 구매빈도, TO_DATE(MAX((purdate)))-TO_DATE(MIN((purdate))) 계속구매기간,
+       (ROUND((SUM(puramt)/COUNT(puramt)),0) * COUNT(puramt) * ((TO_DATE(MAX(purdate)))-(TO_DATE(MIN(purdate))))) CLTV
+FROM purprd
+GROUP BY CUSTNO
+ORDER BY CLTV DESC;
+
+SELECT * FROM VW_CLTV;
+
+--CLTV RANK 구하기
+SELECT 고객번호, 평균구매단가, 구매빈도, 계속구매기간,CLTV,
+       RANK() OVER (ORDER BY CLTV DESC) as CLTV_RANK
+FROM VW_CLTV
+ORDER BY CLTV_RANK;
 
 
 SELECT * FROM channel;
 SELECT * FROM competitor;
 SELECT * FROM purprd;
+SELECT * FROM PRODCL;
+
+SELECT CHANNEL.CUSTNO, CHNNEL.ASSO, PRODCL.ASSO, PRODCL.MCL
+FROM CHANNEL, PRODCL,PURPRD
+WHERE CHANNEL.CUSTNO=PURPRD.CUSTNO
+AND PURPRD.ASSO=PRODCL.ASSO;
