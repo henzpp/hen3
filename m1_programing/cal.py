ef8163ad0254dd6d6df13fce4fe84b998a76a62e
hen77 <lynn5863@hotmail.com>
2022-10-13 17:47:17 +0900

    1013

---
+++ m1_programing/cal.py
@@ -0,0 +1,5 @@
+
+def calcu(x,y):
+    return x + y, x*y, x-y, x/y
+if __name__ == "__main__":
+    print(calcu(3,4))
---
+++ m1_programing/cal4.py
@@ -0,0 +1,4 @@
+
+def cal(x,y):
+    return x + y, x * y, x - y, round(x / y,2)
+print(cal(5,3))
